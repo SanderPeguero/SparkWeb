5925a2dd8224a5ba5ee2cd18b64ab94c75d65e0b
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2023-01-18 17:19:38 -0400

    init develop

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -15,7 +15,7 @@ const Cards = () => {
           </h1>
           <p>Solicite soporte personalizado.</p>
         </div>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-4 gap-8'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-2'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>1 hora</h2>
@@ -25,7 +25,7 @@ const Cards = () => {
                   <p className='py-2 border-b mx-8'>1 Granted User</p>
                   <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
               </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-[#00df9a] w-[200px]  rounded-md font-medium my-6 mx-auto px-6 py-3 '>Start Trial</button>
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
