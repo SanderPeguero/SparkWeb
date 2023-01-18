41cd46787789219156c464732da18998d4ed9574
Sander Peguero <sanderpeguero.21@gmail.com>
2023-01-17 20:52:15 -0400

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+// import './App.css'
+
+//components
+import Analytics from './components/Analytics'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+
+function App() {
+
+  return (
+    <div>
+      <header></header>
+      <main>
+        <Navbar/>
+        <Hero/>
+        <Analytics/>
+        <Cards/>
+        <Newsletter/>
+        <Footer/>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
