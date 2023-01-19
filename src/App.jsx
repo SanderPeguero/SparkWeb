3b9036cc3a9d2808238af72777273661a241f767
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-01-19 10:30:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/NavBar/Navbar'
 import Newsletter from './components/Newsletter'
-import Map from './components/Maps/Map';
 
 //components
 import Home from './components/Home/Home';
@@ -32,7 +31,6 @@ function App() {
           <Newsletter />
           <Cards />
           <Footer /> 
-          {/* <Map/> */}
           <Routes>
             {/* <Route exact path='/' element={<Home />}/>
             <Route exact path='/Analytics' element={<Analytics />}/>
