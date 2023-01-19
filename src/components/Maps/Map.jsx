5aa1ec646be6f0b610609bd03f4141e62e3568a8
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-01-19 09:52:20 -0400

    Rename component Map

---
+++ src/components/Maps/Map.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {GoogleMap, useLoadScript, Marker} from 'react-google-maps';
+
+
+const Map = (props) => {
+    return(
+        <></>
+
+    );
+};
+
+export default Map;
