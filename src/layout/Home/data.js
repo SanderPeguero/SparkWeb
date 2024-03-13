c781cb77372b1cea9f4cf98358f0e2f10d64cf04
Julio Cesar <jimeno.juliocesar@gmail.com>
2024-03-13 16:56:34 -0400

    SG-5-All-images

---
+++ src/layout/Home/data.js
@@ -0,0 +1,165 @@
+// Wallpapers
+import img1 from '../../../public/Wallpapers/1.jpg'
+import img2 from '../../../public/Wallpapers/2.jfif'
+import img3 from '../../../public/Wallpapers/3.jpg'
+import img4 from '../../../public/Wallpapers/4.jpg'
+import img5 from '../../../public/Wallpapers/5.jpg'
+import img6 from '../../../public/Wallpapers/6.jpg'
+import img7 from '../../../public/Wallpapers/7.png'
+import img8 from '../../../public/Wallpapers/8.jfif'
+import img9 from '../../../public/Wallpapers/9.jpg'
+import img10 from '../../../public/Wallpapers/10.jfif'
+import img11 from '../../../public/Wallpapers/11.jfif'
+import img12 from '../../../public/Wallpapers/12.jpg'
+import img13 from '../../../public/Wallpapers/13.jpg'
+import img14 from '../../../public/Wallpapers/14.jpg'
+import img15 from '../../../public/Wallpapers/15.jpg'
+
+
+//Profile
+import prof1 from '../../../public/profile/1.jpg'
+import prof2 from '../../../public/profile/2.png'
+import prof3 from '../../../public/profile/3.png'
+import prof4 from '../../../public/profile/4.jpg'
+import prof5 from '../../../public/profile/5.jpg'
+
+
+ const all =  [
+    {
+        id: "wallpaper 1",
+        src: img1,
+        user: {
+            src: prof1,
+            name: "Arash",
+            job: "Designer"
+        }
+    },
+    {
+        id: "wallpaper 2",
+        src: img2,
+        user: {
+            src: prof2,
+            name: "Farbod",
+            job: "Scientist"
+        }
+    },
+    {
+        id: "wallpaper 3",
+        src: img3,
+        user: {
+            src: prof3,
+            name: "Anne",
+            job: "Graphic Designer"
+        }
+    },
+    {
+        id: "wallpaper 4",
+        src: img4,
+        user: {
+            src: prof4,
+            name: "James",
+            job: "editor"
+        }
+    },
+    {
+        id: "wallpaper 5",
+        src: img5,
+        user: {
+            "src": prof5,
+            "name": "Mary",
+            "job": "Developer"
+        }
+    },
+    {
+        id: "wallpaper 6",
+        src: img6,
+        user: {
+            src: prof3,
+            name: "Patricia",
+            job: "Front-End"
+        }
+    },
+    {
+        id: "wallpaper 7",
+        src: img7,
+        user: {
+            src: prof4,
+            name: "Jennifer",
+            job: "Front-End"
+        }
+    },
+    {
+        id: "wallpaper 8",
+        src: img8,
+        user: {
+            src: prof1,
+            name: "Richard",
+            job: "Product Designer"
+        }
+    },
+    {
+        id: "wallpaper 9",
+        src: img9,
+        user: {
+            src: prof2,
+            name: "Joseph",
+            job: "Designer"
+        }
+    },
+    {
+        id: "wallpaper 10",
+        src: img10,
+        user: {
+            src: prof4,
+            name: "Charles",
+            job: "Designer"
+        }
+    },
+    {
+        id: "wallpaper 11",
+        src: img11,
+        user: {
+            src: prof2,
+            name: "Joshua",
+            job: "Developer"
+        }
+    },
+    {
+        id: "wallpaper 12",
+        src: img12,
+        user: {
+            src: prof5,
+            name: "Jacob",
+            job: "Ui/Ux Designer"
+        }
+    },
+    {
+        id: "wallpaper 13",
+        src: img13,
+        user: {
+            src: prof2,
+            name: "Larry",
+            job: "photographer"
+        }
+    },
+    {
+        id: "wallpaper 14",
+        src: img14,
+        user: {
+            src: prof4,
+            name: "Eric",
+            job: "Editor"
+        }
+    },
+    {
+        id: "wallpaper 15",
+        src: img15,
+        user: {
+            src: prof5,
+            name: "Justin",
+            job: "Designer"
+        }
+    }
+]
+
+export default all;
--- src/layout/Home/home.jsx
+++ src/layout/Home/home.jsx
@@ -5,28 +5,82 @@ import Hero from '../../components/Hero/Hero';
 // import Hero2 from '../../components/Hero/Hero2'
 import Newsletter from '../../components/Newsletter'
 import Features from '../../components/Features/Features';
+import AllImagesLayout from '../../components/AllImagesLayout/AllImagesLayout';
+import ContainerCard from '../../components/ContainerCard/CointainerCard';
+import Dropdown from '../../components/Dropdown/Dropdown';
+import styles from "./home.module.css"
 // import MapComponent from '../Maps/MapComponent';
 // import Footer from '../Footer';
 
 // import LandingPage from '../../assets/Bg.png'
 
-function home(){
+import allimg from './data.js'
+// import images from '../../Jsons/Images.json'
+const ddItems = [
+    {
+        id: 1,
+        title: "All Images",
+        active: true
+    },
+    {
+        id: 2,
+        title: "Topic Images",
+        active: false
+    },
+    {
+        id: 3,
+        title: "Nature Images",
+        active: false
+    },
+    {
+        id: 4,
+        title: "NFT Images",
+        active: false
+    },
+    {
+        id: 5,
+        title: "Space Images",
+        active: false
+    }
+]
 
+function home() {
+    const [categoryImage, setCategoryImage] = useState(allimg)
     const [nicol, setinput] = useState('');
     const [state, setstate] = useState('');
 
     const handle = (e) => {
-       setstate(e.target.value) 
+        setstate(e.target.value)
     }
 
-    return(
+    const takeDdTitle = (ddTitle) => {
+        setCategoryImage(() => {
+            let categoryChoose = allimg.filter(item => {
+                const titleSplited = ddTitle.toLowerCase().split(" ")[0]
+                return item.toLowerCase().includes(titleSplited)
+            })
+            return [...allimg[categoryChoose]]
+        })
+    }
+
+
+
+    return (
         <>
             <Hero />
+            <ContainerCard>
+                <div className={`${styles["gallery-setting"]} flex justify-content-between align-items-center`}>
+                    <h1>All images</h1>
+                    <Dropdown title="All Images" items={ddItems} liftingDdTextUp={takeDdTitle} />
+                </div>
+                <AllImagesLayout images={categoryImage} />
+            </ContainerCard>
+            {/* <AllImagesLayout/> */}
             {/* <Hero2 /> */}
             {/* <Contacto /> */}
             {/* <Analytics /> */}
             {/* <Newsletter />  */}
-            <Features/>
+            <Features />
             {/* <Cards /> */}
             {/* <MapComponent /> */}
             {/* <Footer />  */}
