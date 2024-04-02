56a03240bef78bae0cc5929b083daec3d67b7fd7
Julio <Jimeno.juliocesar@gmail.com>
2024-04-02 11:24:17 -0400

    CTA

--- src/layout/Home/home.jsx
+++ src/layout/Home/home.jsx
@@ -18,6 +18,8 @@ import { ContextVariable } from '../../Context.js';
 
 import allimg from './data.js'
 import images from '../../Jsons/Images.json'
+
+
 const ddItems = [
     {
         id: 1,
@@ -47,17 +49,17 @@ const ddItems = [
 ]
 
 function home() {
-    const {GalleryVisible, setGalleryVisible} = useContext(ContextVariable)
+    const { GalleryVisible, setGalleryVisible } = useContext(ContextVariable)
     const [nicol, setinput] = useState('');
     const [state, setstate] = useState('');
     const galleryRef = useRef(null);
 
     useEffect(() => {
         if (GalleryVisible && galleryRef.current) {
-          galleryRef.current.scrollIntoView({ behavior: 'smooth' });
-          setGalleryVisible(false)
+            galleryRef.current.scrollIntoView({ behavior: 'smooth' });
+            setGalleryVisible(false)
         }
-      }, [GalleryVisible]);
+    }, [GalleryVisible]);
 
     const handle = (e) => {
         setstate(e.target.value)
@@ -96,6 +98,35 @@ function home() {
             <Hero1 />
             <Features />
             <Slider />
+            
+            {/* CTA */}
+
+            <div class="container my-1 mx-auto md:px-6">
+
+                <section >
+                    <div
+                        class="relative overflow-hidden bg-cover bg-no-repeat bg-[40%] bg-[url('https://drive.google.com/file/d/1ipnaKoOlzuFmwW6_A9yl0j8LBNWarCEm/view?usp=sharing')] h-[500px]">
+                        <div
+                            class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.75)] bg-fixed">
+                            <div class="flex h-full items-center justify-center">
+                                <div class="px-6 text-center text-white md:px-12">
+                                    <h2 class="mb-5 text-4xl font-bold leading-tight tracking-tight">
+                                        ¡Sé parte de la vibrante experiencia " <span class="text-primary dark:text-primary-400">Away and Spark</span><span>"!<br />Consigue tus boletas hoy.</span>
+                                    </h2>
+                                    <button onClick={(e) => reserveTicket(true)} className="group relative h-12 w-48 overflow-hidden rounded-xl bg-gradient-to-r from-[#9340FF] to-[#ba36ba] text-lg font-bold text-white my-4">
+                                        Compra ya!
+                                        <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+
+            {/* end CTA */}
+
             <div ref={galleryRef} className="flex justify-content-center" style={{ marginTop: "50px", padding: '50px' }}>
                 <ContainerCard>
                     <div className={`${styles["gallery-setting"]} flex justify-content-between align-items-center`}>
